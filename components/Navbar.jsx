d7ec90e7c7f008be8ab0eb11354b89ca3f1e3d7a
Himanshu-vishwas <himanshuvishwas58@gmail.com>
2023-02-15 18:35:09 +0530

    Changes to be committed: 	modified:   components/Navbar.jsx
    Changes not staged for commit:
    	modified:   styles/globals.css

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -3,6 +3,8 @@
 import { motion } from 'framer-motion';
 import styles from '../styles';
 import { navVariants } from '../utils/motion';
+import { ExploreCard } from '../components';
+
 
 const handleClickScroll =() =>{
   console.log("soon")
@@ -32,7 +34,6 @@ const handleClickScroll4 =() =>{
   }
 }
 
-
 const Navbar = () => (
   <motion.nav
     variants={navVariants}
@@ -44,25 +45,19 @@ const Navbar = () => (
     <div
       className={`${styles.innerWidth} mx-auto flex justify-between items-center gap-8`}
     >
-      {/* <img
-        src="/search.svg"
-        alt="search"
-        className="w-[24px] h-[24px] object-contain"
-      /> */}
      <h2 className="font-extrabold text-[24px] leading-[30
 .24px] text-white items-center" >
         TECHNORITI'23
       </h2>
       <div className='nav-menu'>
-      <li onClick={() => console.log('Clicked!')}>List Item</li>
-
-        <li id='about'  href="www.google.com" >About</li>
+        
+        <li id='about' class="nav-about">About</li>
         <li id='events'>Events</li>
         <li id='participate'>Participate</li>
         <li id='rules'>Rules</li>
       </div>
       <div className='nav-button'>
-        <div className='nav-contact'>Contact Us</div>
+        <button className='nav-contact' id="nav-contact-button">Contact Us</button>
      </div>
     </div>
   </motion.nav>
