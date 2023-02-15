3fe1790c9537c6b9011b129bd6067ee9389cae28
Himanshu-vishwas <himanshuvishwas58@gmail.com>
2023-02-15 17:20:07 +0530

    On branch master Changes to be committed: 	modified:   components/Navbar.jsx 	modified:   sections/Explore.jsx 	modified:   styles/globals.css

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,9 +1,37 @@
 'use client';
 
 import { motion } from 'framer-motion';
-
 import styles from '../styles';
 import { navVariants } from '../utils/motion';
+
+const handleClickScroll =() =>{
+  console.log("soon")
+  const element = document.getElementById('explore');
+  if (element){
+    element.scrollIntoView({behavior:'smooth'});
+  }
+}
+const handleClickScroll2 =() =>{
+  const element = document.getElementById('events');
+  if (element){
+    element.scrollIntoView({behavior:'smooth'});
+  }
+}
+
+const handleClickScroll3 =() =>{
+  const element = document.getElementById('participate');
+  if (element){
+    element.scrollIntoView({behavior:'smooth'});
+  }
+}
+
+const handleClickScroll4 =() =>{
+  const element = document.getElementById('rules');
+  if (element){
+    element.scrollIntoView({behavior:'smooth'});
+  }
+}
+
 
 const Navbar = () => (
   <motion.nav
@@ -14,27 +42,28 @@ const Navbar = () => (
   >
     <div className="absolute w-[50%] inset-0 gradient-01" />
     <div
-      className={`${styles.innerWidth} mx-auto flex justify-between gap-8`}
+      className={`${styles.innerWidth} mx-auto flex justify-between items-center gap-8`}
     >
       {/* <img
         src="/search.svg"
         alt="search"
         className="w-[24px] h-[24px] object-contain"
       /> */}
-      <h2 className="font-extrabold text-[24px] leading-[30.24px] text-white">
+     <h2 className="font-extrabold text-[24px] leading-[30
+.24px] text-white items-center" >
         TECHNORITI'23
       </h2>
       <div className='nav-menu'>
-        <li>About</li>
-        <li>Events</li>
-        <li>Participate</li>
-        <li>Rules</li>
+      <li onClick={() => console.log('Clicked!')}>List Item</li>
+
+        <li id='about'  href="www.google.com" >About</li>
+        <li id='events'>Events</li>
+        <li id='participate'>Participate</li>
+        <li id='rules'>Rules</li>
       </div>
-      <img
-        src="/menu.svg"
-        alt="menu"
-        className="w-[24px] h-[24px] object-contain"
-      />
+      <div className='nav-button'>
+        <div className='nav-contact'>Contact Us</div>
+     </div>
     </div>
   </motion.nav>
 );
